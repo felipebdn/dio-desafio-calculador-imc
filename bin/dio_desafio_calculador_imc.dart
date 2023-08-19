7da165f1785468c8348bf02d9c62efce9c4b8af4
Felipe Barbosa do Nascimento <dfelipebdn@gmail.com>
2023-08-19 08:38:58 -0300

    inital commit

---
+++ bin/dio_desafio_calculador_imc.dart
@@ -0,0 +1,5 @@
+import 'package:dio_desafio_calculador_imc/dio_desafio_calculador_imc.dart' as dio_desafio_calculador_imc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dio_desafio_calculador_imc.calculate()}!');
+}
---
+++ test/dio_desafio_calculador_imc_test.dart
@@ -0,0 +1,8 @@
+import 'package:dio_desafio_calculador_imc/dio_desafio_calculador_imc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
